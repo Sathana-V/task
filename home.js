9805e915edd161996098e7525a83139a39f7a8e1
Sathana-V <69391766+Sathana-V@users.noreply.github.com>
2020-11-29 12:51:51 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('a[href*=#]').bind('click', function(e) {
+            e.preventDefault();
+
+            var target = $(this).attr("href");
+
+           
+            $('html, body').stop().animate({
+                    scrollTop: $(target).offset().top
+            }, 600, function() {
+                    location.hash = target; 
+
+            return false;
+    });
+});
+
